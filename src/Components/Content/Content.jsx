6f3fbf736fcb0e61723ff49a135a531090f71e0e
Dariush <d.bakhtvar@gmail.com>
2023-01-29 23:44:35 +0330

    content style and chart-js2 costomize style

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,16 +1,85 @@
 import React from 'react'
 import SearchBar from '../SearchBar/SearchBar'
 import style from './Content.module.scss';
+// import faker from 'faker';
+import { Doughnut, Line } from "react-chartjs-2";
+import { Chart, ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Filler, Legend, } from "chart.js";
 import { HiPlusSm, HiOutlineChevronDoubleRight } from 'react-icons/hi';
 import { FiArrowDownRight, FiArrowUpRight } from 'react-icons/fi'
+Chart.register(ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Filler, Legend);
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: '',// hiden chart title
+      position: 'left'
+    },
+    tooltip: {
+      enabled: true,
+      intersect: false,
+      mode: 'nearest',
+      yAlign: 'bottom',
+      callbacks: {
+        labelColor: function (tooltipItem, chart) {
+          return {
+            backgroundColor: '#625BC9',
+          }
+        },
+      },
+      backgroundColor: 'rgba(0,0,0,0.7)',
+    },
+    title: {
+      display: true,// hiden chart title
+      text: 'Daily Analytics',
+      position: 'top',
+      align: 'start',
+      padding: {
+        top: 15,
+        bottom: 30,
+      }
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+const Linedata = {
+  labels,
+  datasets: [
+    {
+      fill: true,
+      label: '',
+      data: [200, 600, 180, 200, 450, 500, 370],
+      // data: labels.map(() => faker.random.number({ min: 0, max: 1000 })),
+      // labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      borderColor: '#625BC9',
+      backgroundColor: '#625bc917',
+    },
+  ],
+};
 const Content = ({ isDark, onChangeTheme }) => {
+  const data = {
+    datasets: [
+      {
+        data: [50, 35, 100],
+        backgroundColor: [
+          "#625BC9",
+          "#21CC9E",
+          '#EFF4F8',
+        ],
+        display: true,
+        borderColor: '#F7F0F5',
+      }
+    ]
+  };
+
   return (
     <main className='Content'>
       <SearchBar isDark={isDark} onClickHandler={() => onChangeTheme()} />
       {/* welcome section */}
       <section className={style.WellcomeMassage}>
         <div className={style.TextInfo}>
-          <h4>hi,dariush bakhtvar</h4>
+          <h3>hi,dariush bakhtvar</h3>
           <p>welcome back to budget</p>
         </div>
         <button className={style.newBudgetBtn}>
@@ -18,8 +87,37 @@ const Content = ({ isDark, onChangeTheme }) => {
           <span><HiPlusSm /></span>
         </button>
       </section>
-      <section className={style.TransactionChart}>
-        <aside className={style.sideChart}>
+      {/* Transanction Detail and chanrt */}
+      <section className={style.TotalTransanction}>
+        <section className={style.TransactionBox}>
+          <div className={style.totoalBudget}>
+            <div className={style.totoalBudgetInfo}>
+              <div className={style.totoalBudgetDetail}>
+                <p className={style.IncomeText}>Total Income</p>
+                <h2>545472</h2>
+                <p>increase compeared to last Week</p>
+              </div>
+              <span><FiArrowUpRight /></span>
+            </div>
+            <button className={style.NoneBtnBg}>
+              <span>See Details</span>
+              <span><HiOutlineChevronDoubleRight /></span>
+            </button>
+          </div>
+          <div className={style.totoalBudget}>
+            <div className={style.totoalBudgetInfo}>
+              <div className={style.totoalBudgetDetail}>
+                <p className={style.ExpenseText}>Total Expense</p>
+                <h2>545472</h2>
+                <p>increase compared to last Week</p>
+              </div>
+              <span className={style.colorRed}><FiArrowDownRight /></span>
+            </div>
+            <button className={style.NoneBtnBg}>
+              <span>See Details</span>
+              <span><HiOutlineChevronDoubleRight /></span>
+            </button>
+          </div>
           <div className={style.totoalBudget}>
             <div className={style.totoalBudgetInfo}>
               <div className={style.totoalBudgetDetail}>
@@ -34,47 +132,43 @@ const Content = ({ isDark, onChangeTheme }) => {
               <span><HiOutlineChevronDoubleRight /></span>
             </button>
           </div>
-          <div className={style.totoalChart}>
-            <p>test</p>
-          </div>
-        </aside>
-        <div className={style.DetailChart}>
-          <div className={style.DetailBudget}>
-            {/* inCome section */}
-            <div className={style.totoalBudget}>
-              <div className={style.totoalBudgetInfo}>
-                <div className={style.totoalBudgetDetail}>
-                  <p className={style.IncomeText}>Total Income</p>
-                  <h2>545472</h2>
-                  <p>increase compeared to last Week</p>
-                </div>
-                <span><FiArrowUpRight /></span>
-              </div>
-              <button className={style.NoneBtnBg}>
-                <span>See Details</span>
-                <span><HiOutlineChevronDoubleRight /></span>
-              </button>
-            </div>
-            {/* Expense section */}
-            <div className={style.totoalBudget}>
-              <div className={style.totoalBudgetInfo}>
-                <div className={style.totoalBudgetDetail}>
-                  <p className={style.ExpenseText}>Total Expense</p>
-                  <h2>545472</h2>
-                  <p>increase compared to last Week</p>
-                </div>
-                <span className={style.colorRed}><FiArrowDownRight /></span>
-              </div>
-              <button className={style.NoneBtnBg}>
-                <span>See Details</span>
-                <span><HiOutlineChevronDoubleRight /></span>
-              </button>
-            </div>
-          </div>
+        </section>
+        <section className={style.TransactionCharts}>
           <div className={style.DetailHistogram}>
-            <p>test</p>
+            <Line options={options} data={Linedata} />
           </div>
-        </div>
+          <div className={style.totoalChart}>
+            <Doughnut
+              data={data}
+              options={{
+                plugins: {
+                  legend: {
+                    display: false
+                  },
+                  tooltip: {
+                    enabled: false
+                  }
+                },
+                rotation: -90,
+                circumference: 180,
+                cutout: "60%",
+                maintainAspectRatio: true,
+                responsive: true
+              }}
+            />
+            <div
+              style={{
+                position: "absolute",
+                top: "55%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                textAlign: "center"
+              }}
+            >
+              <div>Text Here</div>
+            </div>
+          </div>
+        </section>
       </section>
     </main>
   )
